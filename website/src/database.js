2916ea0cdfdb9307efc8592c1180808478866298
mathiasmagnusson <mathiasmagnussons@gmail.com>
2020-01-28 23:06:04 +0100

    basic database structure for accounts, problems and submissions

--- website/src/database.js
+++ website/src/database.js
@@ -3,21 +3,109 @@ import creds from "./creds.json";
 
 class Database {
 	constructor() {
+		this.connect();
+
 		[
 			`CREATE TABLE IF NOT EXISTS account (
 				id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+
 				username VARCHAR(20) NOT NULL,
 				email VARCHAR(40) NULL,
 				password VARCHAR(60) NOT NULL
 			)`,
 			`CREATE TABLE IF NOT EXISTS problem (
 				id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+
 				short_name VARCHAR(30) NOT NULL,
 				long_name VARCHAR(40),
+				author_id INT NOT NULL,
+
+				testing_method ENUM('run-script', 'compare-output') NOT NULL,
+				test_case_count INT NOT NULL,
+
 				time_limit_ms INT NOT NULL,
-				memory_limit_mb INT NOT NULL
+				memory_limit_mb INT NOT NULL,
+
+				description TEXT NOT NULL,
+
+				# test cases, validation script...
+
+				FOREIGN KEY (author_id) REFERENCES account(id)
 			)`,
+			`CREATE TABLE IF NOT EXISTS submission (
+				id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+
+				problem_id INT NOT NULL,
+				account_id INT NOT NULL,
+
+				time INT NOT NULL, # unix timestamp
+
+				lang VARCHAR(4) NOT NULL, # file ending, like: c, cc, rs, js, hs, java
+
+				source TEXT NOT NULL,
+
+				status ENUM(
+					'pending',
+					'accepted',
+					'wrong-answer',
+					'runtime-error',
+					'compilation-error',
+					'time-limit-exceeded',
+					'memory-limit-exceeded'
+				) NOT NULL,
+				max_runtime_ms INT, # null when status is pending or compilation-error
+				max_memory_mb INT, # null when status is pending or compilation-error
+				test_cases_succeeded INT, # null when status is pending or compilation-error
+
+				FOREIGN KEY (problem_id) REFERENCES problem(id),
+				FOREIGN KEY (account_id) REFERENCES account(id)
+			)`,
+
 		]
+			.forEach(async (query, i) => {
+				try {
+					await this.query(query);
+				}
+				catch (err) {
+					console.error(`Error creating database table ${i}:`);
+					console.error(err);
+					process.exit(-1);
+				}
+			});
+	}
+
+	connect() {
+		const { host, user, password, database } = creds.mysql;
+
+		this.inner = mysql.createConnection({
+			host,
+			user,
+			password,
+			database,
+		});
+
+		this.inner.connect(err => {
+			if (err) {
+				console.error("inner.connect err: ");
+				console.error(err);
+				setTimeout(() => this.connect(), 5000);
+			}
+		});
+
+		this.inner.on("error", err => {
+			console.error("inner.on(\"error\"): ");
+			console.error(err);
+			setTimeout(() => this.console(), 5000);
+		});
+	}
+
+	query(query, args) {
+		return new Promise((resolve, reject) => {
+			this.inner.query(query, args, (err, result) => {
+				if (err) reject(err);
+				else resolve(result);
+			});
+		});
 	}
 }
 
