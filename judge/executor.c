f4c9d4fa503aff0661d87bdaa85a01078d830316
mathiasmagnusson <mathiasmagnussons@gmail.com>
2020-03-30 00:48:34 +0200

    fett me text jaoo

--- judge/executor.c
+++ judge/executor.c
@@ -7,66 +7,50 @@
 #include <string.h>
 
 // build:
-	// gcc -static -O3 -o executor executor.c
+	// gcc -O3 -o executor executor.c
 	// strip executor
-	// sudo chown root:root executor
-	// sudo chmod 4755 executor
+	// sudo chown root:judge-master executor
+	// sudo chmod 4750 executor
 
 #define DIRECTORY 1
 #define NEW_UID 2
 #define PROGRAM_NAME 3
 #define REQ_ARGS 3
 
+#define die(...) { fprintf(stderr, __VA_ARGS__); return -1; }
+
 int main(int argc, char** argv) {
-	if (argc < 4) {
-		fprintf(
-			stderr,
-			"Syntax: %s <directory> <new uid> <program name> <args...>\n",
-			argv[0]
-		);
-		return -1;
-	}
+	if (argc < 4) die(
+		"Syntax: %s <directory> <uid> <program> <args...>\n",
+		argv[0]
+	);
 
-	if (setuid(0)) {
-		fprintf(stderr, "Could not be root\n");
-		return -1;
-	}
+	if (chdir(argv[DIRECTORY]))
+		die("Could not change directory to %s\n", argv[1]);
 
-	if (chdir(argv[DIRECTORY])) {
-		fprintf(stderr, "Could not change directory to %s\n", argv[1]);
-	}
-
-	if (chroot(".")) {
-		char cwd[512];
-		if (!getcwd(cwd, 512)) {
-			cwd[0] = '?';
-			cwd[1] = '\0';
-		}
-		fprintf(stderr, "Could not set root dir to %s\n", cwd);
-		return -1;
-	}
+	if (chroot("."))
+		die("Could not set root dir to\n");
 
 	int uid = atoi(argv[NEW_UID]);
 
-	if (setuid(uid)) {
-		fprintf(stderr, "Could not set uid to %d\n", uid);
-		return -1;
-	}
+	if (uid == 0) die("Refusing to run as root");
+
+	if (setuid(uid))
+		die("Could not set uid to %d\n", uid);
 
 	char** child_argv = malloc(
-		sizeof (char*) *
-		(argc - REQ_ARGS + 1)
+		sizeof (char*) * (argc - REQ_ARGS + 1)
 	);
 	child_argv[0] = argv[PROGRAM_NAME];
 	int i;
 	for (i = 1; i < argc - REQ_ARGS; i++) {
-		child_argv[i] = malloc(sizeof (char) * (strlen(argv[i + REQ_ARGS]) + 1));
+		child_argv[i] = malloc(
+			sizeof (char) * (strlen(argv[i + REQ_ARGS]) + 1)
+		);
 		strcpy(child_argv[i], argv[i + REQ_ARGS]);
 	}
 	child_argv[i] = NULL;
 
-	int res = execve(argv[PROGRAM_NAME], child_argv, 0);
-
-	fprintf(stderr, "execve returned (failed) errno: %d\n", errno);
-	return -1;
+	execve(argv[PROGRAM_NAME], child_argv, 0);
+	die("execve returned (failed) errno: %d\n", errno);
 }
